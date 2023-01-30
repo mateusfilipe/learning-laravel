5f5e970dc1f7501f66c8474920debd5edcfff67e
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-30 17:00:11 -0300

    Testes iniciais com laravel e o blade

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = 'Mateus';
+    $idade = 22;
+
+    $arr = [1,2,3,4,5];
+
+    return view('welcome', 
+    [
+        'nome' => $nome, 
+        'idade' => $idade, 
+        'profissao' => 'Programador',
+        'arr' => $arr,
+    ]);
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
+
+
