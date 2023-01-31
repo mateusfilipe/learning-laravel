6c8d841b3bd0e258baf38a17337b6dc9b8c02ebf
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-31 09:03:06 -0300

    Add css, bootstrap e fonts ao projeto

--- hdcevents/resources/views/layouts/main.blade.php
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -5,10 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>@yield('title')</title>
+        
         {{-- Fonte do Google --}}
-
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+        
         {{-- CSS Bootstrap --}}
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        
         {{-- CSS da aplicação --}}
         <link rel="stylesheet" href="/css/styles.css">
         <script src="/js/scripts.js"></script>
