c2abdb8878df758d22c33598a3f78f31c665d872
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-31 17:13:27 -0300

    Add logica para busca de eventos

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -9,10 +9,16 @@ class EventController extends Controller
 {
     public function index()
     {
+        $search = request('search');
 
-        $events = Event::all();
-
-        return view('welcome', ['events' => $events]);
+        if ($search) {
+            $events = Event::where([
+                ['title', 'like', '%' . $search . '%'],
+            ])->get();
+        } else {
+            $events = Event::all();
+        }
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create()
@@ -25,9 +31,11 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         //Image Upload
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
