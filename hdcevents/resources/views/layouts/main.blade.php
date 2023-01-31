303ce80380f9ed763b2c16084d9d439896b1181c
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-31 10:27:44 -0300

    Add navbar e bootstrap ao projeto

--- hdcevents/resources/views/layouts/main.blade.php
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -13,12 +13,41 @@
         
         {{-- CSS Bootstrap --}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
         {{-- CSS da aplicação --}}
-        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/css/styles.css"></script>
         <script src="/js/scripts.js"></script>
     </head>
     <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="/">
+                    <img width="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png" alt="Pokeball">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="/">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/">Criar Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/">Cadastrar</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+        </header>
     @yield('content')
     <footer>
         <p>HDC Events &copy; 2023</p>
