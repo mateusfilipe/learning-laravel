8ef17ddb749657a6488a67f32ecd2e55d26edfd9
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-02-02 10:50:32 -0300

    Add table de eventos

--- hdcevents/resources/views/events/dashboard.blade.php
+++ hdcevents/resources/views/events/dashboard.blade.php
@@ -9,6 +9,26 @@
 </div>
 <div class="col-md-10 offset-md-1 dashboard-events-container">
     @if(count($events) > 0)
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Participantes</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($events as $event)
+                <tr>
+                    <td scropt='row'>{{ $loop->index + 1 }}</td>
+                    <td> <a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                    <td>0</td>
+                    <td><a href="#">Editar</a> <a href="#">Deletar</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
     @else
         <p>Você ainda não possui evenhum evento. <a href="/events/create">Crie seu evento.</a></p>
     @endif
