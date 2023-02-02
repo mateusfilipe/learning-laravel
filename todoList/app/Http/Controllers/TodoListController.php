c217c01230cdf1da475444616f9e07569f7b3e58
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-02-02 17:47:58 -0300

    Init project todo list laravel

---
+++ todoList/app/Http/Controllers/TodoListController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoListController extends Controller
+{
+    //
+    public function saveItem()
+    {
+        return view('welcome');
+    }
+}
